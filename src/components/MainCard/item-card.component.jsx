0e1ef76c5c073dab7e4752a00c014c0508ca504f
claudiogs16 <claudiogs16@gmail.com>
2022-06-20 16:19:56 -0100

    Modal Problem Solved

--- src/components/MainCard/item-card.component.jsx
+++ src/components/MainCard/item-card.component.jsx
@@ -2,7 +2,7 @@ import { Button, Card, CardContent, Typography } from "@mui/material";
 
 const ItemCard = props => {
     return (
-        <Card elevation={0} style={{background: '#fafafa',  borderRadius: '15px', textAlign: 'center'}}>
+        <Card elevation={0} style={{background: '#F9FAFE',  borderRadius: '15px', textAlign: 'center'}}>
             <CardContent>
                 <Typography variant="h6" style={{fontWeight: 'bold', opacity: 0.6}}>{props.value}</Typography>
                 <Button variant="text" size="small">{props.title}</Button>
--- src/pages/DefinitionPage/menu-item.jsx
+++ src/pages/DefinitionPage/menu-item.jsx
@@ -1,14 +1,42 @@
-import { Button, Card, CardContent } from "@mui/material"
+import { Button, Card, CardContent } from "@mui/material";
+import { useState } from "react";
+import Modal from "../../components/Modal/modal.component";
 
-const MenuItem = props => {
-    return(
-        <Card elevation={0} style={{borderRadius: '15px', backgroundColor: '#fafafa'}}>
-            <CardContent style={{textAlign: 'center'}}>
-                {props.icon}
-                <Button onClick={props.handleClickOpen} variant="text" size="small" fullWidth>{props.title}</Button>
-            </CardContent>
-        </Card>
-    )
-}
+const MenuItem = (props) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Modal open={open} onClose={handleClose} maxWidth="lg" fullScreen={true}>
+        {props.modalContent}
+      </Modal>
+
+      <Card
+        elevation={0}
+        style={{ borderRadius: "15px", backgroundColor: "#F9FAFE" }}
+      >
+        <CardContent style={{ textAlign: "center" }}>
+          {props.icon}
+          <Button
+            onClick={handleClickOpen}
+            variant="text"
+            size="small"
+            fullWidth
+          >
+            {props.title}
+          </Button>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
 
 export default MenuItem;
